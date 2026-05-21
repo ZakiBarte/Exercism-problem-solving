2f263b04807087ad3b42b37ee98bf53cbedd52bf
Coding Time <zakibarte1@gmail.com>
2026-05-21 13:28:56 +0300

    done first problem and cloning the second problem ' the lasagna ' ---

---
+++ lasagna/index.js
@@ -0,0 +1,15 @@
+const numberOfLayers = 2
+const EXPECTED_MINUTES_IN_OVEN = 40
+
+
+
+preparationTimeInMinutes(numberOfLayers);
+remainingMinutesInOven(20)
+
+
+const remainingMinutesInOven = (20) = {
+   
+    
+}
+
+
